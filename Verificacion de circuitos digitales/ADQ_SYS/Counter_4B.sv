413fe35376081e3809499aa74e9fe32d79c743f3
DaniRodAg <daniel.rodriguez0017@alumnos.udg.mx>
2024-09-28 00:28:04 -0600

    Simulation and verification of Data acquisition system

---
+++ Verificacion de circuitos digitales/ADQ_SYS/Counter_4B.sv
@@ -0,0 +1,26 @@
+module Counter_8B(
+  input wire clk, rst,
+  output reg [7:0] COUNT,
+  output reg fac
+);
+
+always @(posedge clk or posedge rst) 
+begin
+  if (rst) 
+  begin
+    COUNT <= 'h0;
+    fac = 'b0;
+  end 
+  else 
+  begin
+    if(COUNT=='hFF) 
+    begin
+      COUNT = 'h00;
+      fac = 'b1;
+    end
+    else COUNT = COUNT+1;
+  end
+end
+
+
+endmodule
